340cdcf7802f19649753c1909dee26088cc78726
无绘 <61873808+MliKiowa@users.noreply.github.com>
2021-07-27 14:05:26 +0800

    Add files via upload

--- Action.php
+++ Action.php
@@ -20,7 +20,7 @@ class GithubStatic_Action extends Typecho_Widget implements Widget_Interface_Do
      // $this->on($this->request->is('do=ConfigCheck'))->ConfigCheck(); //二次验证
     }
   public function  Recache($login_config){
-     $this->is_pass()
+     $this->is_pass();
      $_options=empty($login_config)?Helper::options()->plugin('GithubFile"):$login_config;     
       $file_repos = _TMP_PATH . "/repos.json";
       if(file_exists($file_repos)){unlink($file_repos);}
@@ -32,7 +32,7 @@ class GithubStatic_Action extends Typecho_Widget implements Widget_Interface_Do
       exit;
      }
       public function GithubAuth(){
-         $this->is_pass()
+         $this->is_pass();
          $username = Github_user_login($this->request->from('token')["token"])->login;
          if(empty($username)){          
            die( '哇噗, 你的授权失败了,因为登录获取用户名失败'  );
