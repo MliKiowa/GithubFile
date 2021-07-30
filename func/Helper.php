3b91443a8fdc82c9101f482923e473e284e78c2e
无绘 <61873808+MliKiowa@users.noreply.github.com>
2021-07-30 18:02:00 +0800

    规范

--- func/Helper.php
+++ func/Helper.php
@@ -1,26 +1,26 @@
 <?php
-function _Get_config($name,$default){
-         $_db = Typecho_Db::get();
-         $result = $_db->fetchAll($_db->select('value')->from('table.options')->where('name = ?', "plugin:GithubFile"));        
-         if(!isset($result[0]["value"])){return $default;}
-         $_options=unserialize($result[0]["value"]);
-         return (isset($_options[$name])?$_options[$name]:$default);        
+function _Get_config($name, $default) {
+    $_db = Typecho_Db::get();
+    $result = $_db->fetchAll($_db->select('value')->from('table.options')->where('name = ?', 'plugin:GithubFile'));
+    if (!isset($result[0]['value'])) {
+        return $default;
+    }
+    $_options = unserialize($result[0]['value']);
+    return (isset($_options[$name]) ? $_options[$name] : $default);
 }
-function _Get_tmp_repos($tmp_path){
-if (@file_exists( $tmp_path."/repos.json" ) ) {
-            $_file = fopen( $tmp_path.'/repos.json', 'r' );
-            $_repos = ( array )json_decode( fread( $_file, filesize($tmp_path.'/repos.json' ) ) );
-            fclose( $_file );
-                 $repos=array();
-          if(isset($_repos["msg"])){return array(""=>"");}
-            foreach ( $_repos as $key => $value ) {
-
-                   $repos = array_merge($repos, array( ($value->name) =>($value->name ) ));
-                }
-                return $repos;
-                
-          }          
-          return array(""=>"");
+function _Get_tmp_repos($tmp_path) {
+    if (@file_exists($tmp_path . '/repos.json')) {
+        $_file = fopen($tmp_path . '/repos.json', 'r');
+        $_repos = ( array )json_decode(fread($_file, filesize($tmp_path . '/repos.json')));
+        fclose($_file);
+        $repos = array();
+        if (isset($_repos['msg'])) {
+            return array('' => '');
+        }
+        foreach ($_repos as $key => $value) {
+            $repos = array_merge($repos, array(($value->name) => ($value->name)));
+        }
+        return $repos;
+    }
+    return array('' => '');
 }
-                
-                
