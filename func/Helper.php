c9117d26250d6e438815f3dcc5ec562ad5976b58
无绘 <61873808+MliKiowa@users.noreply.github.com>
2021-07-30 13:08:08 +0800

    修复一个默认值问题

--- func/Helper.php
+++ func/Helper.php
@@ -2,7 +2,7 @@
 function _Get_config($name,$default){
          $_db = Typecho_Db::get();
          $result = $_db->fetchAll($_db->select('value')->from('table.options')->where('name = ?', "plugin:GithubFile"));        
-         if(!isset($result[0]["value"])){return "";}
+         if(!isset($result[0]["value"])){return $default;}
          $_options=unserialize($result[0]["value"]);
          return (isset($_options[$name])?$_options[$name]:$default);        
 }
