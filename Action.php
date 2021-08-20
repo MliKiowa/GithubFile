b7c1802a2b859967a243e213a3f59549fb9450e7
无绘 <61873808+MliKiowa@users.noreply.github.com>
2021-08-20 10:16:45 +0800

    代码整理
    优化格式

--- Action.php
+++ Action.php
@@ -1,70 +1,76 @@
 <?php
 /**
- * Action
- *
- * @author : Mlikiowa
- * @time : 2021.8.30
- * @version : 1.0.2
- */
-defined("_TMP_PATH") or define("_TMP_PATH", dirname(__FILE__) . "/tmp");
-defined("_Cache_PATH") or define("_Cache_PATH", dirname(__FILE__) . "/cache");
-require_once dirname(__FILE__) . "/class/GithubApi.php";
-require_once dirname(__FILE__) . "/func/Helper.php";
+* Action
+*
+* @author : Mlikiowa
+* @time : 2021.8.30
+* @version : 1.0.2
+*/
+defined( '_TMP_PATH' ) or define( '_TMP_PATH', dirname( __FILE__ ) . '/tmp' );
+defined( '_Cache_PATH' ) or define( '_Cache_PATH', dirname( __FILE__ ) . '/cache' );
+require_once dirname( __FILE__ ) . '/class/GithubApi.php';
+require_once dirname( __FILE__ ) . '/func/Helper.php';
+
 class GithubFile_Action extends Typecho_Widget implements Widget_Interface_Do {
     public function action() {
-        $this->on($this->request->is("do=Recache"))->Recache();
-        $this->on($this->request->is("do=GithubAuth"))->GithubAuth();
-        $this->on($this->request->is("do=del_log"))->del_log();
-     // $this->on($this->request->is("do=ConfigCheck"))->ConfigCheck(); //二次验证
-        
+        $this->on( $this->request->is( 'do=Recache' ) )->Recache();
+        $this->on( $this->request->is( 'do=GithubAuth' ) )->GithubAuth();
+        $this->on( $this->request->is( 'do=del_log' ) )->del_log();
+        // $this->on( $this->request->is( 'do=ConfigCheck' ) )->ConfigCheck();
+        //二次验证
+
     }
+
     public function del_log() {
         $this->is_pass();
-        $filename = $this->request->from("filename") ["filename"];
-        @unlink( __DIR__."/cache/".  $filename);
-       /** 提示信息 */
-        $this->widget('Widget_Notice')->set(_t("文件已经删除"), 'success');
+        $filename = $this->request->from( 'filename' ) ['filename'];
+        @unlink( __DIR__.'/cache/'.  $filename );
+        /** 提示信息 */
+        $this->widget( 'Widget_Notice' )->set( _t( '文件已经删除' ), 'success' );
         /** 转向原页 */
         $this->response->goBack();
-        }
+    }
+
     public function Recache() {
         $this->is_pass();
-        $_options = Helper::options()->plugin("GithubFile");
-        $file_repos = _TMP_PATH . "/repos.json";
-        if (file_exists($file_repos)) {
-            unlink($file_repos);
+        $_options = Helper::options()->plugin( 'GithubFile' );
+        $file_repos = _TMP_PATH . '/repos.json';
+        if ( file_exists( $file_repos ) ) {
+            unlink( $file_repos );
         }
-        $file_repos = fopen($file_repos, "w+");
+        $file_repos = fopen( $file_repos, 'w+' );
         $api = new GithubApi();
-        $api->set_api(_Get_config("mirror", "https://api.github.com"));
-        $api->set_token($_options->token);
-        fwrite($file_repos, $api->repos_all($_options->username));
-        fclose($file_repos);
-        header("HTTP/1.1 301 Moved Permanently");
-        header("Location: /admin/options-plugin.php?config=GithubFile");
+        $api->set_api( _Get_config( 'mirror', 'https://api.github.com' ) );
+        $api->set_token( $_options->token );
+        fwrite( $file_repos, $api->repos_all( $_options->username ) );
+        fclose( $file_repos );
+        header( 'HTTP/1.1 301 Moved Permanently' );
+        header( 'Location: /admin/options-plugin.php?config=GithubFile' );
         exit;
     }
+
     public function GithubAuth() {
         $this->is_pass();
         $api = new GithubApi();
-        $api->set_api(_Get_config("mirror", "https://api.github.com"));
-        $api->set_token($this->request->from("token") ["token"]);
+        $api->set_api( _Get_config( 'mirror', 'https://api.github.com' ) );
+        $api->set_token( $this->request->from( 'token' ) ['token'] );
         $username = $api->user_login()->login;
-        if (empty($username)) {
-            die("哇噗, 你的授权失败了,因为登录获取用户名失败");
+        if ( empty( $username ) ) {
+            die( '哇噗, 你的授权失败了,因为登录获取用户名失败' );
         }
-        $_options = array("token" => $this->request->from("token") ["token"], "username" => $username);
-        Helper::configPlugin("GithubFile", $_options);
-        header("HTTP/1.1 301 Moved Permanently");
-        header("Location: /action/GithubFile?do=Recache");
+        $_options = array( 'token' => $this->request->from( 'token' ) ['token'], 'username' => $username );
+        Helper::configPlugin( 'GithubFile', $_options );
+        header( 'HTTP/1.1 301 Moved Permanently' );
+        header( 'Location: /action/GithubFile?do=Recache' );
         //此处使用跳转缘由 Helper::options()没有刷新
         //修改无跳转
-        
+
     }
+
     public function is_pass() {
-        $user = Typecho_Widget::widget("Widget_User");
-        if (!$user->pass("administrator")) {
-            die("未登录用户!");
+        $user = Typecho_Widget::widget( 'Widget_User' );
+        if ( !$user->pass( 'administrator' ) ) {
+            die( '未登录用户!' );
         }
     }
 }
--- class/Plugin_Handler.php
+++ class/Plugin_Handler.php
@@ -1,105 +1,106 @@
 <?php
-require_once dirname(dirname(__FILE__)) . '/func/Helper.php';
+require_once dirname( dirname( __FILE__ ) ) . '/func/Helper.php';
 require_once 'GithubApi.php';
+
 class Plugin_Handler extends Typecho_Widget {
-    public static function uploadHandle($file) {
-        $options = Typecho_Widget::widget('Widget_Options')->plugin('GithubFile');
-        if (empty($file['name'])) return false;
+    public static function uploadHandle( $file ) {
+        $options = Typecho_Widget::widget( 'Widget_Options' )->plugin( 'GithubFile' );
+        if ( empty( $file['name'] ) ) return false;
         //获取扩展名
-        $ext = self::getSafeName($file['name']);
+        $ext = self::getSafeName( $file['name'] );
         //判定是否是允许的文件类型
-        if (!Widget_Upload::checkFileType($ext)) return false;
+        if ( !Widget_Upload::checkFileType( $ext ) ) return false;
         //获取文件名 如果需要可修改规则
         //注意流
-        $filePath = date('Y') . '/' . date('m') . '/' . date('d') . '/';
+        $filePath = date( 'Y' ) . '/' . date( 'm' ) . '/' . date( 'd' ) . '/';
         $fileName = time() . '.' . $ext;
         //上传文件的路径+名称
         $newPath = $filePath . $fileName;
         //获取插件参数
         //如果没有临时文件，则使用流上传
-        if (isset($file['tmp_name'])) {
+        if ( isset( $file['tmp_name'] ) ) {
             $srcPath = $file['tmp_name'];
-            $handle = fopen($srcPath, 'r');
-            $contents = fread($handle, $file['size']);
-        } else if (isset($file['bytes'])) {
+            $handle = fopen( $srcPath, 'r' );
+            $contents = fread( $handle, $file['size'] );
+        } else if ( isset( $file['bytes'] ) ) {
             $contents = $file['bytes'];
-        } else if (isset($file['bits'])) {
+        } else if ( isset( $file['bits'] ) ) {
             $contents = $file['bits'];
         } else {
             return false;
         }
-        if (!isset($file['size'])) {
-            $file['size'] = filesize($file['tmp_name']);
+        if ( !isset( $file['size'] ) ) {
+            $file['size'] = filesize( $file['tmp_name'] );
         }
         $api = new GithubApi();
-        $result = $api->set_api(_Get_config('mirror', 'https://api.github.com'));
-        $api->set_token(_Get_config('token', ''));
+        $result = $api->set_api( _Get_config( 'mirror', 'https://api.github.com' ) );
+        $api->set_token( _Get_config( 'token', '' ) );
         //$contents 获取二进制数据流
-        if (!$api->files_upload($options->username, $options->repo, $options->path . $newPath, $contents)) {
-            $api->files_updata($options->username, $options->repo, $options->path . $newPath, $contents, $api->get_sha($options->username, $options->repo, $options->path . $newPath));
+        if ( !$api->files_upload( $options->username, $options->repo, $options->path . $newPath, $contents ) ) {
+            $api->files_updata( $options->username, $options->repo, $options->path . $newPath, $contents, $api->get_sha( $options->username, $options->repo, $options->path . $newPath ) );
         }
         //使用newPath并不连接$options->path URL连接时拼接
-        return array('name' => $file['name'], 'path' => $newPath, 'size' => $file['size'], 'type' => $ext, 'mime' => isset($file['mime']) ? $file['mime'] : Typecho_Common::mimeContentType($newPath),);
+        return array( 'name' => $file['name'], 'path' => $newPath, 'size' => $file['size'], 'type' => $ext, 'mime' => isset( $file['mime'] ) ? $file['mime'] : Typecho_Common::mimeContentType( $newPath ), );
     }
-    private static function getSafeName(&$name) {
+    private static function getSafeName( &$name ) {
         $name = str_replace( array( '"', '<', '>' ), '', $name );
         $name = str_replace( '\\', '/', $name );
-        $name = false === strpos($name, '/') ? ('a' . $name) : str_replace('/', '/a', $name);
-        $info = pathinfo($name);
-        $name = substr($info['basename'], 1);
-        return isset($info['extension']) ? strtolower($info['extension']) : '';
+        $name = false === strpos( $name, '/' ) ? ( 'a' . $name ) : str_replace( '/', '/a', $name );
+        $info = pathinfo( $name );
+        $name = substr( $info['basename'], 1 );
+        return isset( $info['extension'] ) ? strtolower( $info['extension'] ) : '';
     }
-    public static function attachmentDataHandle($content) {
-        $options = Typecho_Widget::widget('Widget_Options')->plugin('GithubFile');
+    public static function attachmentDataHandle( $content ) {
+        $options = Typecho_Widget::widget( 'Widget_Options' )->plugin( 'GithubFile' );
         //获取设置参数
-        return Typecho_Common::url($content['attachment']->path, _Get_config('cdn', 'https://cdn.jsdelivr.net/gh/') . $options->username . '/' . $options->repo . $options->path);
+        return Typecho_Common::url( $content['attachment']->path, _Get_config( 'cdn', 'https://cdn.jsdelivr.net/gh/' ) . $options->username . '/' . $options->repo . $options->path );
     }
-    public static function deleteHandle(array $content) {
-        $options = Typecho_Widget::widget('Widget_Options')->plugin('GithubFile');
+    public static function deleteHandle( array $content ) {
+        $options = Typecho_Widget::widget( 'Widget_Options' )->plugin( 'GithubFile' );
         $api = new GithubApi();
-        $result = $api->set_api(_Get_config('mirror', 'https://api.github.com'));
-        $api->set_token(_Get_config('token', ''));
-        $ret = $api->files_del($options->username, $options->repo, $options->path . $content['attachment']->path, $api->get_sha($options->username, $options->repo, $options->path . $content['attachment']->path));
+        $result = $api->set_api( _Get_config( 'mirror', 'https://api.github.com' ) );
+        $api->set_token( _Get_config( 'token', '' ) );
+        $ret = $api->files_del( $options->username, $options->repo, $options->path . $content['attachment']->path, $api->get_sha( $options->username, $options->repo, $options->path . $content['attachment']->path ) );
         return $ret;
     }
-    public static function attachmentHandle(array $content) {
-        $options = Typecho_Widget::widget('Widget_Options')->plugin('GithubFile');
-        return Typecho_Common::url($content['attachment']->path, _Get_config('cdn', 'https://cdn.jsdelivr.net/gh/') . $options->username . '/' . $options->repo . $options->path);
+    public static function attachmentHandle( array $content ) {
+        $options = Typecho_Widget::widget( 'Widget_Options' )->plugin( 'GithubFile' );
+        return Typecho_Common::url( $content['attachment']->path, _Get_config( 'cdn', 'https://cdn.jsdelivr.net/gh/' ) . $options->username . '/' . $options->repo . $options->path );
     }
-    public static function modifyHandle($content, $file) {
-        $options = Typecho_Widget::widget('Widget_Options')->plugin('GithubFile');
-        if (empty($file['name'])) return false;
+    public static function modifyHandle( $content, $file ) {
+        $options = Typecho_Widget::widget( 'Widget_Options' )->plugin( 'GithubFile' );
+        if ( empty( $file['name'] ) ) return false;
         //获取扩展名
-        $ext = self::getSafeName($file['name']);
+        $ext = self::getSafeName( $file['name'] );
         //判定是否是允许的文件类型
-        if (!Widget_Upload::checkFileType($ext)) return false;
+        if ( !Widget_Upload::checkFileType( $ext ) ) return false;
         //获取设置参数
-        $options = Typecho_Widget::widget('Widget_Options')->plugin('GithubStatic');
+        $options = Typecho_Widget::widget( 'Widget_Options' )->plugin( 'GithubStatic' );
         //获取文件路径
         $path = $content['attachment']->path;
         //获得上传文件
-        if (isset($file['tmp_name'])) {
+        if ( isset( $file['tmp_name'] ) ) {
             $srcPath = $file['tmp_name'];
-            $handle = fopen($srcPath, 'r');
-            $contents = fread($handle, $file['size']);
-        } else if (isset($file['bytes'])) {
+            $handle = fopen( $srcPath, 'r' );
+            $contents = fread( $handle, $file['size'] );
+        } else if ( isset( $file['bytes'] ) ) {
             $contents = $file['bytes'];
-        } else if (isset($file['bits'])) {
+        } else if ( isset( $file['bits'] ) ) {
             $contents = $file['bits'];
         } else {
             return false;
         }
-        if (!isset($file['size'])) {
-            $file['size'] = filesize($file['tmp_name']);
+        if ( !isset( $file['size'] ) ) {
+            $file['size'] = filesize( $file['tmp_name'] );
         }
         //$contents 获取二进制数据流
         $api = new GithubApi();
-        $result = $api->set_api(_Get_config('mirror', 'https://api.github.com'));
-        $api->set_token(_Get_config('token', ''));
-        if (!$api->files_upload($options->username, $options->repo, $options->path . $newPath, $contents)) {
-            $api->files_updata($options->username, $options->repo, $options->path . $newPath, $contents, $api->get_sha($options->username, $options->repo, $options->path . $newPath));
+        $result = $api->set_api( _Get_config( 'mirror', 'https://api.github.com' ) );
+        $api->set_token( _Get_config( 'token', '' ) );
+        if ( !$api->files_upload( $options->username, $options->repo, $options->path . $newPath, $contents ) ) {
+            $api->files_updata( $options->username, $options->repo, $options->path . $newPath, $contents, $api->get_sha( $options->username, $options->repo, $options->path . $newPath ) );
         }
         //使用newPath并不连接$options->path URL连接时拼接
-        return array('name' => $file['name'], 'path' => $Path, 'size' => $file['size'], 'type' => $ext, 'mime' => isset($file['mime']) ? $file['mime'] : Typecho_Common::mimeContentType($Path),);
+        return array( 'name' => $file['name'], 'path' => $Path, 'size' => $file['size'], 'type' => $ext, 'mime' => isset( $file['mime'] ) ? $file['mime'] : Typecho_Common::mimeContentType( $Path ), );
     }
 }
