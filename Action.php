6d7e8c6230e514b4be333a45e08a59c11a5d948f
无绘 <nineto0@163.com>
2022-06-18 18:12:00 +0800

    Add files via upload

---
+++ Action.php
@@ -0,0 +1,53 @@
+<?php
+/**
+* Action
+*
+* @author : Mlikiowa
+* @time : 2021.8.30
+* @version : 1.0.2
+*/
+defined( '_TMP_PATH' ) or define( '_TMP_PATH', dirname( __FILE__ ) . '/cache/tmp' );
+defined( '_Cache_PATH' ) or define( '_Cache_PATH', dirname( __FILE__ ) . '/cache' );
+defined( '_Log_Path' ) or define( '_Log_Path', dirname( __FILE__ ) . '/cache/need' );
+
+class GithubFile_Action extends Typecho_Widget implements Widget_Interface_Do {
+    public function action() {
+        $this->on( $this->request->is( 'do=Recache' ) )->Recache();
+        $this->on( $this->request->is( 'do=DelLog' ) )->DelLog();
+    }
+
+    public function del_log() {
+        $this->is_pass();
+        $filename = $this->request->from( 'filename' ) ['filename'];
+        @unlink( __DIR__.'/cache/'.  $filename );
+        /** 提示信息 */
+        $this->widget( 'Widget_Notice' )->set( _t( '文件已经删除' ), 'success' );
+        /** 转向原页 */
+        $this->response->goBack();
+    }
+
+    public function Recache() {
+        $this->is_pass();
+        $_options = Helper::options()->plugin( 'GithubFile' );
+        $file_repos = _TMP_PATH . '/repos.json';
+        if ( file_exists( $file_repos ) ) {
+            unlink( $file_repos );
+        }
+        $file_repos = fopen( $file_repos, 'w+' );
+        GithubFile_Api::set_api( GithubFile_Helper::GetConfig( 'Mirror', 'https://api.github.com' ) );
+        GithubFile_Api::SetUser( GithubFile_Helper::GetConfig( 'Username', '' ),GithubFile_Helper::GetConfig( 'Password', '' ) );
+        fwrite( $file_repos, GithubFile_Api::ReposAll( $_options->Username ) );
+        fclose( $file_repos );
+        header( 'HTTP/1.1 301 Moved Permanently' );
+        header( 'Location: /admin/options-plugin.php?config=GithubFile' );
+        exit;
+    }
+
+
+    public function is_pass() {
+        $user = Typecho_Widget::widget( 'Widget_User' );
+        if ( !$user->pass( 'administrator' ) ) {
+            die( '未登录用户!' );
+        }
+    }
+}
---
+++ Api.php
@@ -0,0 +1,60 @@
+<?php
+if (!defined('__TYPECHO_ROOT_DIR__')) {
+	exit;
+}
+
+class GithubFile_Api implements Typecho_Plugin_Interface {
+	private $_Username;
+	private $_Password;
+	private $_Api;
+	public static  function SetUser(string $Username, string $Password) {
+		$this->_Username = $Username;
+		$this->_Password = $Password;
+	}
+	public static function SetApi(string $Api) {
+		$this->_Api = $Api;
+	}
+	public static function SendApi(string $Url = '', string $Data = '', string $Menthod = 'GET') {
+		$http = Typecho_Http_Client::get();
+		$http->setMethod($Method);
+		$http->setHeader('User-Agent', 'GithubFile PluginApi2 ');
+		$http->setHeader('Authorization', 'Basic ' . base64_encode($this->_Username . ':' . $this->_Password));
+		$http->setData($Data);
+		$result = $http->send($this->Api . $Url);
+		return $result;
+	}
+	public static public function UserInfo(string $Username) {
+		return json_decode(self::SendApi('/users/' . $Username));
+	}
+	public static function UserLogin() {
+		return json_decode(self::SendApi('/user'));
+	}
+	public static function ReposAll(string $Username) {
+		return self::SendApi('/users/' . $Username . '/repos');
+	}
+	public static function ReposInfo(string $Username, string $ReposName) {
+		return json_decode(self::SendApi('/repos/' . $Username . '/' . $ReposName));
+	}
+	public static function ReposPath(string $Username, string $ReposName, string $Path) {
+		return json_decode(self::SendApi('/repos/' . $Username . '/' . $ReposName . '/contents' . $Path));
+	}
+	public static function FilesUpload(string $Username, string $Repos, string $Path, string $Files) {
+		$data = array('message' => 'Upload-GithubFile', 'content' => base64_encode($files));
+		$json = (array) json_decode(self::SendApi('/repos/' . $Username . '/' . $Repos . '/contents' . $Path, json_encode($data), 'PUT'));
+		return !isset($json['message']);
+	}
+	public static function FilesUpdata(string $Username, string $Repos, string $Path, string $Files, string $Sha) {
+		$data = array('message' => 'Updata-GithubFile', 'content' => base64_encode($files), 'sha' => $Sha);
+		$json = (array) json_decode(self::SendApi('/repos/' . $Username . '/' . $Repos . '/contents' . $Path, json_encode($data), 'PUT'));
+		return !isset($json['message']);
+	}
+	public static function FilesDel(string $Username, string $Repos, string $Path, string $Sha) {
+		$data = array('message' => 'Del-GithubFile', 'sha' => $Sha);
+		$json = (array) json_decode(self::SendApi('/repos/' . $Username . '/' . $Repos . '/contents' . $Path, json_encode($data), 'DELETE'));
+		return !isset($json['message']);
+	}
+	public static function GetSha(string $Username, string $Repos, string $Path) {
+		$json = (array) self::ReposPath($Username, $Repos, $Path);
+		return $json['sha'];
+	}
+}
