37c5ec8f174e9956cf834064865ce48c3adea1c5
无绘 <nineto0@163.com>
2022-06-20 19:47:16 +0800

    Update Action.php

--- Action.php
+++ Action.php
@@ -1,15 +1,7 @@
 <?php
-/**
-* Action
-*
-* @author : Mlikiowa
-* @time : 2021.8.30
-* @version : 1.0.2
-*/
-defined( '_TMP_PATH' ) or define( '_TMP_PATH', dirname( __FILE__ ) . '/cache/tmp' );
-defined( '_Cache_PATH' ) or define( '_Cache_PATH', dirname( __FILE__ ) . '/cache' );
-defined( '_Log_Path' ) or define( '_Log_Path', dirname( __FILE__ ) . '/cache/need' );
-
+defined('_TMP_PATH') or define('_TMP_PATH', dirname(__FILE__) . '/cache/tmp');
+defined('_Cache_PATH') or define('_Cache_PATH', dirname(__FILE__) . '/cache');
+defined('_Log_Path') or define('_Log_Path', dirname(__FILE__) . '/cache/need');
 class GithubFile_Action extends Typecho_Widget implements Widget_Interface_Do {
     public function action() {
         $this->on( $this->request->is( 'do=Recache' ) )->Recache();
@@ -19,10 +11,9 @@ class GithubFile_Action extends Typecho_Widget implements Widget_Interface_Do {
     public function del_log() {
         $this->is_pass();
         $filename = $this->request->from( 'filename' ) ['filename'];
-        @unlink( __DIR__.'/cache/'.  $filename );
-        /** 提示信息 */
+        @unlink( __DIR__.'/cache/'.  $filename );        
         $this->widget( 'Widget_Notice' )->set( _t( '文件已经删除' ), 'success' );
-        /** 转向原页 */
+        
         $this->response->goBack();
     }
 
@@ -35,19 +26,17 @@ class GithubFile_Action extends Typecho_Widget implements Widget_Interface_Do {
         }
         $file_repos = fopen( $file_repos, 'w+' );
         GithubFile_Api::set_api( GithubFile_Helper::GetConfig( 'Mirror', 'https://api.github.com' ) );
-        GithubFile_Api::SetUser( GithubFile_Helper::GetConfig( 'Username', '' ),GithubFile_Helper::GetConfig( 'Password', '' ) );
+GithubFile_Api::SetUser( GithubFile_Helper::GetConfig( 'Username', '' ),GithubFile_Helper::GetConfig( 'Password', '' ) );
         fwrite( $file_repos, GithubFile_Api::ReposAll( $_options->Username ) );
         fclose( $file_repos );
         header( 'HTTP/1.1 301 Moved Permanently' );
         header( 'Location: /admin/options-plugin.php?config=GithubFile' );
         exit;
     }
-
-
     public function is_pass() {
         $user = Typecho_Widget::widget( 'Widget_User' );
         if ( !$user->pass( 'administrator' ) ) {
             die( '未登录用户!' );
         }
     }
-}
+} 
