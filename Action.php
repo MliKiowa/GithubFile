7d7043b168fcf2cc911ecb35f9f5263987249cd5
MliKiowa <nineto0@163.com>
2022-10-16 12:33:13 +0800

    fix : log path

--- Action.php
+++ Action.php
@@ -7,9 +7,9 @@
  * @version 1.1.0
  * @since 1.0.0
  */
-defined('_CACHE_PATH') or define('_CACHE_PATH', dirname(__FILE__) . '/cache');
-defined('_TMP_PATH') or define('_TMP_PATH', dirname(__FILE__) . '/cache/tmp');
-defined('_LOG_PATH') or define('_LOG_PATH', dirname(__FILE__) . '/cache/log');
+defined('_CACHE_PATH') or define('_CACHE_PATH', dirname(__FILE__) . '/cache/');
+defined('_TMP_PATH') or define('_TMP_PATH', dirname(__FILE__) . '/cache/tmp/');
+defined('_LOG_PATH') or define('_LOG_PATH', dirname(__FILE__) . '/cache/log/');
 
 class GithubFile_Action extends Typecho_Widget implements Widget_Interface_Do
 {
@@ -35,7 +35,7 @@ class GithubFile_Action extends Typecho_Widget implements Widget_Interface_Do
     {
         $this->isPass();
         $filename = $this->request->from('filename') ['filename'];
-        @unlink(__DIR__ . '/cache/' . $filename);
+        @unlink(_LOG_PATH . 'cache/' . $filename);
         $this->widget('Widget_Notice')->set(_t('文件已经删除'), 'success');
         $this->response->goBack();
     }
@@ -52,30 +52,5 @@ class GithubFile_Action extends Typecho_Widget implements Widget_Interface_Do
         if (!$user->pass('administrator')) {
             die('未登录用户!');
         }
-    }
-
-    /**
-     * Notes:
-     * User: Mlikiowa<nineto0@163.com>
-     * Date: 2022-06-22
-     * Time:17:03
-     */
-    public function reCache():void
-    {
-        $this->isPass();
-        $_options = Helper::options()->plugin('GithubFile');
-        $file_repos = _TMP_PATH . '/repos.json';
-        if (file_exists($file_repos)) {
-            unlink($file_repos);
-        }
-        $file_repos = fopen($file_repos, 'w+');
-        $Api = new GithubFile_Api();
-        $Api->setApi(GithubFile_Helper::GetConfig('Mirror', 'https://api.github.com'));
-        $Api->SetUser(GithubFile_Helper::GetConfig('Username', ''), GithubFile_Helper::GetConfig('token', ''));
-        fwrite($file_repos, $Api->getReposAll($_options->Username));
-        fclose($file_repos);
-        header('HTTP/1.1 301 Moved Permanently');
-        header('Location: /admin/options-plugin.php?config=GithubFile');
-        exit;
     }
 } 
