72d14e4fb768dfc3b962079b5afa0a2c2e87ed08
MliKiowa <nineto0@163.com>
2022-10-16 12:38:07 +0800

    Update LogList.php

--- LogList.php
+++ LogList.php
@@ -6,9 +6,13 @@
  * @version 1.1.0
  * @since 1.0.0
  */
+defined('_CACHE_PATH') or define('_CACHE_PATH', dirname(__FILE__) . '/cache/');
+defined('_TMP_PATH') or define('_TMP_PATH', dirname(__FILE__) . '/cache/tmp/');
+defined('_LOG_PATH') or define('_LOG_PATH', dirname(__FILE__) . '/cache/log/');
+
 function getTable($tid, $filename): string
 {
-    return '<tr id="log-' . $tid . '"><td >' . $filename . '</td><td><a href="/usr/plugins/GithubFile/cache/need/' . $filename . '">下载</a>&bull;<a lang="你确认要删除吗?" href="/action/GithubFile?do=del_log&filename=' . $filename . '">删除</a></td></tr>';
+    return '<tr id="log-' . $tid . '"><td >' . $filename . '</td><td><a href="/usr/plugins/GithubFile/cache/log/' . $filename . '">下载</a>&bull;<a lang="你确认要删除吗?" href="/action/GithubFile?do=del_log&filename=' . $filename . '">删除</a></td></tr>';
 }
 
 include 'header.php';
@@ -37,11 +41,10 @@ include 'menu.php';
                         <tbody>
                         <?php
                         $count = 0;
-                        $path = __DIR__ . '/cache/need';
-                        $files = scandir($path);
+                        $files = scandir(_TMP_PATH);
                         $fileItem = [];
                         foreach ($files as $v) {
-                            $newPath = $path . DIRECTORY_SEPARATOR . $v;
+                            $newPath = _TMP_PATH . $v;
                             if (is_file($newPath)) {
                                 $count++;
                                 echo getTable($count, $v);
