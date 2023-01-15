fdd76aa9ad64ce8e12c5caa786d9215c833ade43
十雪拾雪 <nineto0@163.com>
2023-01-15 13:52:05 +0800

    Update Handler.php

--- Handler.php
+++ Handler.php
@@ -85,7 +85,7 @@ class Handler {
     public static function attachmentDataHandle($content) {
         $options = \Typecho\Widget::widget('Widget_Options')->plugin('GithubFile');
         //获取设置参数
-        $codearr = array("file"=>$content['attachment']->path,"mirror"=> Helper::GetConfig('Cdn', 'https://fastly.jsdelivr.net/gh/'),"user"=>$options->Username,"repo"=>$options->Repo);
+        $codearr = array("file"=>substr ($content['attachment']->path, 1),"mirror"=> Helper::GetConfig('Cdn', 'https://fastly.jsdelivr.net/gh/'),"user"=>$options->Username,"repo"=>$options->Repo);
         $url = Helper::replaceCode($options->MirroPath,$codearr);
         return $url;
 //废弃代码 Typecho_Common::url($content['attachment']->path, Helper::GetConfig('Cdn', 'https://fastly.jsdelivr.net/gh/') . $options->Username . '/' . $options->Repo . $options->Path);    
@@ -124,7 +124,7 @@ class Handler {
      */
     public static function attachmentHandle(array $content) {
         $options =  \Typecho\Widget::widget('Widget_Options')->plugin('GithubFile');        
-        $codearr = array("file"=>$content['attachment']->path,"cdn"=> Helper::GetConfig('Cdn', 'https://fastly.jsdelivr.net/gh/'),"user"=>$options->Username,"repo"=>$options->Repo);
+        $codearr = array("file"=>substr ($content['attachment']->path, 1),"cdn"=> Helper::GetConfig('Cdn', 'https://fastly.jsdelivr.net/gh/'),"user"=>$options->Username,"repo"=>$options->Repo);
         $url = Helper::replaceCode($options->MirroPath,$codearr);
         return $url;
 //废弃代码 return \Typecho\Common::url($content['attachment']->path, Helper::GetConfig('Cdn', 'https://fastly.jsdelivr.net/gh/') . $options->Username . '/' . $options->Repo . $options->Path);
