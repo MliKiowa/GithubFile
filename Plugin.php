7f8f468c6733cc0a90e5cd11fcb4733184541e62
手瓜一十雪 <nineto0@163.com>
2023-05-01 20:14:01 +0800

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -1,111 +1,83 @@
-<?php
+<?php 
 namespace TypechoPlugin\GithubFile;
-
-use Typecho\Plugin\PluginInterface;
-use Typecho\Widget\Helper\Form;
 use Utils\Helper;
-use Typecho\Widget\Helper\Form\Element\Radio;
-use Typecho\Widget\Helper\Form\Element\Text;
+use Typecho\Widget\Helper\Form;
 use Widget\Options;
-use Typecho\Plugin\Exception as PluginException;
-
-/**
- * Use GitHub Repos To Update Attachment
- *
- * @package GithubFile
- * @author Mlikiowa<nineto0@163.com>
- * @version 1.3.9
- * @license MIT License
- * @link https://github.com/MliKiowa/GithubFile
- */
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
-defined('_CACHE_PATH') or define('_CACHE_PATH', dirname(__FILE__) . '/cache/');
-defined('_TMP_PATH') or define('_TMP_PATH', dirname(__FILE__) . '/cache/tmp/');
-defined('_LOG_PATH') or define('_LOG_PATH', dirname(__FILE__) . '/cache/log/');
-
-class Plugin implements PluginInterface
+class Plugin implements \Typecho\Plugin\PluginInterface
 {
-    public static function personalConfig( Form $form )
-    {
-    }
+    const CACHE_PATH = 'cache';
+    const TMP_PATH = self::CACHE_PATH . '/tmp';
+    const LOG_PATH = self::CACHE_PATH . '/log';
+    /**
+     * @var array 存储路径相关属性
+     */
+    protected static $pathProps = [
+        'repo', 'mirrPath', 'realPath', 'username', 'token'
+    ];
+    /**
+     * 激活插件时初始化相关目录
+     */
     public static function activate()
     {
-        /**
-         * 判断是否可用HTTP库 CURL库
-         * 此处说明，并非使用Typecho_Http_Client，由于并未提供PUT等操作弃用 使用从Typecho抽离修改的库提供支持
-         */
         if (basename(dirname(__FILE__)) !== 'GithubFile') {
-            throw new PluginException(_t('The plugin name must be GithubFile'));
+            throw new \Typecho\Plugin\Exception(_t('The plugin name must be GithubFile'));
         }
-        //生成相关目录
-        if (!file_exists(_CACHE_PATH)) {
-            @mkdir(_CACHE_PATH);  
-            @mkdir(_TMP_PATH);
-            @mkdir(_LOG_PATH);
+        if (!is_dir(self::CACHE_PATH)) {
+            mkdir(self::CACHE_PATH);
+            mkdir(self::TMP_PATH);
+            mkdir(self::LOG_PATH);
         }
-        //挂接钩子
-        Helper::addAction( 'GithubFile', 'GithubFile_Action' );
-        //上传
-        \Typecho\Plugin::factory('Widget_Upload')->uploadHandle = ['GithubFile_Handler', 'uploadHandle'];
-        //修改
-        \Typecho\Plugin::factory('Widget_Upload')->modifyHandle = ['GithubFile_Handler', 'modifyHandle'];
-        //删除
-        \Typecho\Plugin::factory('Widget_Upload')->deleteHandle = ['GithubFile_Handler', 'deleteHandle'];
-        //路径参数处理
-        \Typecho\Plugin::factory('Widget_Upload')->attachmentHandle = ['GithubFile_Handler', 'attachmentHandle'];
-        //文件内容数据
-        \Typecho\Plugin::factory('Widget_Upload')->attachmentDataHandle = ['GithubFile_Handler', 'attachmentDataHandle'];
+        Helper::addAction('GithubFile', [__CLASS__, 'handle']);
+        $uploadFactory = \Typecho\Plugin::factory('Widget_Upload');
+        $uploadFactory->on('uploadHandle', [__NAMESPACE__ . '\GithubFile_Handler', 'uploadHandle']);
+        $uploadFactory->on('modifyHandle', [__NAMESPACE__ . '\GithubFile_Handler', 'modifyHandle']);
+        $uploadFactory->on('deleteHandle', [__NAMESPACE__ . '\GithubFile_Handler', 'deleteHandle']);
+        $uploadFactory->on('attachmentHandle', [__NAMESPACE__ . '\GithubFile_Handler', 'attachmentHandle']);
+        $uploadFactory->on('attachmentDataHandle', [__NAMESPACE__ . '\GithubFile_Handler', 'attachmentDataHandle']);
         return _t('可以使用啦~');
     }
-
     public static function deactivate()
     {
-        if (\Typecho\Widget::widget('Widget_Options')->plugin('GithubFile')->debug_log) {
+        $debugLog = \Typecho\Widget::widget('Widget_Options')->plugin('GithubFile')->debug_log;
+        if ($debugLog) {
             Helper::removePanel(1, 'GithubFile/LogList.php');
         }
         Helper::removeAction('GithubFile');
         return _t('Disabled~');
     }
-
-    public static function configHandle($config, $isInit)
+    public static function handle()
     {
-        if (!$isInit) {
-            if (($config['DebugLog']) !== (\Typecho\Widget::widget('Widget_Options')->plugin('GithubFile')->DebugLog)) {
-                if ($config['DebugLog']) {
-                    Helper::addPanel(1, 'GithubFile/LogList.php', '插件日志', '日志内容', 'administrator');
-                } else {
-                    Helper::removePanel(1, 'GithubFile/LogList.php');
-                }
-            }
+        if (!isset($_POST['_SERVER']) && !isset($_POST['_FILES'])) {
+            throw new \Typecho\Plugin\Exception('请不要手工执行上传！');
         }
-        Helper::configPlugin('GithubFile', $config);
+        return \Typecho\Plugin::factory(GithubFile_Handler::class)->handle();
     }
-
     public static function config(Form $form)
     {
-        //账号储存
-        $t = new Text('Username', null, null, _t('Username'), _t(''));
-        $form->addInput($t->addRule('required', _t('不能为空哦~')));
-        $t = new Text('token', null, null, _t('token'), _t(''));
-        $form->addInput($t->addRule('required', _t('不能为空哦~')));     
-        //储存路径设置
-        $t = new Text('Repo',null, null, _t('仓库名'), _t(''));
-        $form->addInput($t);
+        $form->addInput(new Form\Element\Text('Username', null, null, _t('Username'), _t(''))->addRule('required', _t('不能为空哦~')));
+        $form->addInput(new Form\Element\Text('token', null, null, _t('token'), _t(''))->addRule('required', _t('不能为空哦~')));
+        foreach (self::$pathProps as $key) {
+            $form->addInput(new Form\Element\Text(ucfirst($key), null, null, _t(ucfirst($key)), _t(''))->addRule('required', _t('不能为空哦~')));
+        }
         //调试设置
-        $t = new Radio('DebugLog', array(true => '开启', false => '关闭'), false, _t('调试设置'), _t('启用后记录调试日志'));
+        $t = new Form\Element\Radio('DebugLog', array(true => '开启', false => '关闭'), false, _t('调试设置'), _t('启用后记录调试日志'));
         $form->addInput($t);
         //镜像设置
-        $t = new Text('Mirror', null, 'https://api.github.com', _t('API_Mirror'), _t('加速API提供Mirror'));
+        $t = new Form\Element\Text('Mirror', null, 'https://api.github.com', _t('API_Mirror'), _t('加速API提供Mirror'));
         $form->addInput($t->addRule('required', _t('不能为空哦~')));
-        $t = new Text('Cdn', null, 'https://cdn.zenless.top/gh', _t('File_Mirror'), _t('加速提供文件 结尾不能带有/'));
+        $t = new Form\Element\Text('Cdn', null, 'https://cdn.zenless.top/gh', _t('File_Mirror'), _t('加速提供文件 结尾不能带有/'));
         $form->addInput($t->addRule('required', _t('不能为空哦~')));
         //短代码目录生成
-        $t = new Text('MirroPath', null, '[cdn]/[user]/[repo]/[file]', _t('镜像加速目录规则'), _t('生成的链接设置 提示: 可选[user][repo][file][cdn]'));
+        $t = new Form\Element\Text('MirroPath', null, '[cdn]/[user]/[repo]/[file]', _t('镜像加速目录规则'), _t('生成的链接设置 提示: 可选[user][repo][file][cdn]'));
         $form->addInput($t->addRule('required', _t('不能为空哦~')));
-        $t = new Text('RealPath', null, '/[file]', _t('实际上传目录规则'), _t('上传到github时的目录设置 设置可为/你的目录/[file] 必须包括/[file]'));
+        $t = new Form\Element\Text('RealPath', null, '/[file]', _t('实际上传目录规则'), _t('上传到github时的目录设置 设置可为/你的目录/[file] 必须包括/[file]'));
         $form->addInput($t->addRule('required', _t('不能为空哦~')));
         //图片压缩设置 未实装
-        $t = new Radio('ImgCompress', array(true => '开启', false => '关闭'), false, _t('图片压缩'), _t('未实装 敬请期待'));
+        $t = new Form\Element\Radio('ImgCompress', array(true => '开启', false => '关闭'), false, _t('图片压缩'), _t('未实装 敬请期待'));
         $form->addInput($t);
     }
+    public static function personalConfig(Form $form)
+    {
+    }    
 }
