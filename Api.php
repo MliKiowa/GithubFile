59d4a0155a7ae658e0ea6b5e24e2a7406adc371a
手瓜一十雪 <nineto0@163.com>
2023-05-14 22:38:28 +0800

    优化API HTTP

--- Api.php
+++ Api.php
@@ -1,70 +1,178 @@
 <?php
 namespace TypechoPlugin\GithubFile;
+
 /**
- *插件封装api部分
- * Class类名称(GithubFile_Api)
+ * 单例模式下的插件封装api部分
+ * Class GithubFile_Api
  * @package GithubFile
- * @author Mlikiowa<nineto0@163.com>
- * @version 1.3.2
- * @since 1.0.0
  */
 class Api
 {
-    private  $mtoken;
-    private  $mApi;
+    private static $instance = null; //唯一实例
+
+    private $mtoken; //私有属性，存储token信息
+    private $mApi; //私有属性，存储api链接
 
     /**
-     * Notes:
-     * User: Mlikiowa<nineto0@163.com>
-     * Date: 2022-06-22
-     * Time:17:05
+     * 构造函数私有化，防止外部new实例化
+     * GithubFile_Api constructor.
+     */
+    private function __construct() {}
+
+    /**
+     * 获取唯一实例的公开方法
+     * @return Api|null
+     */
+    public static function getInstance(): ?Api {
+        if (null === self::$instance) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    /**
+     * 设置token信息
      * @param string $token
      */
-    public function setUser(string $token): void
-    {
+    public function setUser(string $token): void {
         $this->mtoken = $token;
     }
 
     /**
-     * Notes:
-     * User: Mlikiowa<nineto0@163.com>
-     * Date: 2022-06-22
-     * Time:17:05
+     * 设置api链接
      * @param string $Api
      */
-    public function setApi(string $Api): void
-    {
+    public function setApi(string $Api): void {
         $this->mApi = $Api;
     }
 
     /**
-     * Notes:
-     * User: Mlikiowa<nineto0@163.com>
-     * Date: 2022-06-22
-     * Time:17:05
+     * 获取用户信息
      * @param string $Username
-     * @return mixed
+     * @return string
      */
-    public function getUserInfo(string $Username): string
-    {
-        return json_decode(self::SendApi('/users/' . $Username));
+    public function getUserInfo(string $Username): string {
+        return json_decode($this->sendApi(sprintf('/users/%s', $Username)));
     }
 
     /**
-     * Notes:
-     * User: Mlikiowa<nineto0@163.com>
-     * Date: 2022-06-22
-     * Time:17:05
-     * @param string $Url
-     * @param string $Data
-     * @param string $Method
+     * 获取用户登录信息
+     * @return string
+     */
+    public function getUserLogin(): string {
+        return json_decode($this->sendApi('/user'));
+    }
+
+    /**
+     * 获取所有仓库信息
+     * @param string $Username
+     * @return string
+     */
+    public function getReposAll(string $Username): string {
+        return $this->sendApi(sprintf('/users/%s/repos', $Username));
+    }
+
+    /**
+     * 获取仓库指定路径的信息
+     * @param string $Username
+     * @param string $ReposName
+     * @param string $Path
      * @return mixed
      */
-    public function sendApi(string $url = '', string $data = '', string $method = 'GET')
-    {
-        $http = \Typecho\Http\Client::get();       
+    public function getReposPath(string $Username, string $ReposName, string $Path) {
+        $temp = $this->sendApi(sprintf('/repos/%s/%s/contents%s', $Username, $ReposName, $Path));
+        return json_decode($temp);
+    }
+
+    /**
+     * 获取指定仓库的信息
+     * @param string $Username
+     * @param string $ReposName
+     * @return string
+     */
+    public function getReposInfo(string $Username, string $ReposName): string {
+        return json_decode($this->sendApi(sprintf('/repos/%s/%s', $Username, $ReposName)));
+    }
+
+    /**
+     * 上传指定路径的文件
+     * @param string $Username
+     * @param string $Repos
+     * @param string $Path
+     * @param string $Files
+     * @return string
+     */
+    public function uploadFiles(string $Username, string $Repos, string $Path, string $Files): string {
+        $data = array(
+            'message' => 'Upload-GithubFile',
+            'content' => base64_encode($Files)
+        );
+
+        $json = (array)json_decode($this->sendApi(sprintf('/repos/%s/%s/contents%s', $Username, $Repos, $Path), json_encode($data), 'PUT'));
+        return !isset($json['message']);
+    }
+
+    /**
+     * 更新指定路径的文件
+     * @param string $Username
+     * @param string $Repos
+     * @param string $Path
+     * @param string $Files
+     * @param string $Sha
+     * @return bool
+     */
+    public function updateFiles(string $Username, string $Repos, string $Path, string $Files, string $Sha): bool {
+        $data = array(
+            'message' => 'Update-GithubFile',
+            'content' => base64_encode($Files),
+            'sha' => $Sha
+        );
+
+        $json = (array)json_decode($this->sendApi(sprintf('/repos/%s/%s/contents%s', $Username, $Repos, $Path), json_encode($data), 'PUT'));
+        return !isset($json['message']);
+    }
+
+    /**
+     * 删除指定路径的文件
+     * @param string $Username
+     * @param string $Repos
+     * @param string $Path
+     * @param string $Sha
+     * @return bool
+     */
+    public function delFiles(string $Username, string $Repos, string $Path, string $Sha): bool {
+        $data = array(
+            'message' => 'Del-GithubFile',
+            'sha' => $Sha
+        );
+
+        $json = (array)json_decode($this->sendApi(sprintf('/repos/%s/%s/contents%s', $Username, $Repos, $Path), json_encode($data), 'DELETE'));
+        return !isset($json['message']);
+    }
+
+    /**
+     * 获取指定路径的sha值
+     * @param string $Username
+     * @param string $Repos
+     * @param string $Path
+     * @return string
+     */
+    public function getSha(string $Username, string $Repos, string $Path): string {
+        $json = (array)$this->getReposPath($Username, $Repos, $Path);
+        return $json['sha'];
+    }
+
+    /**
+     * 发送请求的私有方法
+     * @param string $url 请求链接
+     * @param string $data 请求数据
+     * @param string $method 请求方法
+     * @return mixed
+     */
+    private function sendApi(string $url = '', string $data = '', string $method = 'GET') {
+        $http = \Typecho\Http\Client::get();
         $http->setHeader('User-Agent', 'GithubFile PluginApi2 ');
-        $http->setHeader('Authorization', 'token '  . $this->mtoken);
+        $http->setHeader('Authorization', 'token ' . $this->mtoken);
         $http->setTimeout(40);
         $http->setData($data);
         $http->setMethod($method);
@@ -73,140 +181,8 @@ class Api
     }
 
     /**
-     * Notes:
-     * User: Mlikiowa<nineto0@163.com>
-     * Date: 2022-06-22
-     * Time:17:05
-     * @return mixed
+     * 构造函数私有化，防止克隆对象
      */
-    public function getUserLogin(): string
-    {
-        return json_decode(self::SendApi('/user'));
-    }
+    private function __clone() {}
 
-    /**
-     * Notes:
-     * User: Mlikiowa<nineto0@163.com>
-     * Date: 2022-06-22
-     * Time:17:05
-     * @param string $Username
-     * @return mixed
-     */
-    public function getReposAll(string $Username): string
-    {
-        return self::SendApi('/users/' . $Username . '/repos');
-    }
-
-    /**
-     * Notes:
-     * User: Mlikiowa<nineto0@163.com>
-     * Date: 2022-06-22
-     * Time:17:05
-     * @param string $Username
-     * @param string $ReposName
-     * @return mixed
-     */
-    public function getReposInfo(string $Username, string $ReposName): string
-    {
-        return json_decode(self::SendApi('/repos/' . $Username . '/' . $ReposName));
-    }
-
-    /**
-     * Notes:
-     * User: Mlikiowa<nineto0@163.com>
-     * Date: 2022-06-22
-     * Time:17:05
-     * @param string $Username
-     * @param string $Repos
-     * @param string $Path
-     * @param string $Files
-     * @return string
-     */
-    public function uploadFiles(string $Username, string $Repos, string $Path, string $Files): string
-    {
-        $data = array(
-            'message' => 'Upload-GithubFile',
-            'content' => base64_encode($Files)
-        );
-        $json = (array)json_decode(self::SendApi('/repos/' . $Username . '/' . $Repos . '/contents' . $Path, json_encode($data), 'PUT'));
-        return !isset($json['message']);
-    }
-
-    /**
-     * Notes:
-     * User: Mlikiowa<nineto0@163.com>
-     * Date: 2022-06-22
-     * Time:17:05
-     * @param string $Username
-     * @param string $Repos
-     * @param string $Path
-     * @param string $Files
-     * @param string $Sha
-     * @return bool
-     */
-    public function updateFiles(string $Username, string $Repos, string $Path, string $Files, string $Sha): bool
-    {
-        $data = array(
-            'message' => 'Update-GithubFile',
-            'content' => base64_encode($Files),
-            'sha' => $Sha
-        );
-        $json = (array)json_decode(self::SendApi('/repos/' . $Username . '/' . $Repos . '/contents' . $Path, json_encode($data), 'PUT'));
-        return !isset($json['message']);
-    }
-
-    /**
-     * Notes:
-     * User: Mlikiowa<nineto0@163.com>
-     * Date: 2022-06-22
-     * Time:17:05
-     * @param string $Username
-     * @param string $Repos
-     * @param string $Path
-     * @param string $Sha
-     * @return bool
-     */
-    public function delFiles(string $Username, string $Repos, string $Path, string $Sha): bool
-    {
-        $data = array(
-            'message' => 'Del-GithubFile',
-            'sha' => $Sha
-        );
-        $json = (array)json_decode(self::SendApi('/repos/' . $Username . '/' . $Repos . '/contents' . $Path, json_encode($data), 'DELETE'));
-        return !isset($json['message']);
-    }
-
-    /**
-     * Notes:
-     * User: Mlikiowa<nineto0@163.com>
-     * Date: 2022-06-22
-     * Time:17:05
-     * @param string $Username
-     * @param string $Repos
-     * @param string $Path
-     * @return mixed
-     */
-    public function getSha(string $Username, string $Repos, string $Path): string
-    {
-        $json = (array)$this->getReposPath($Username, $Repos, $Path);
-        return $json['sha'];
-    }
-
-    /**
-     * Notes:
-     * User: Mlikiowa<nineto0@163.com>
-     * Date: 2022-06-22
-     * Time:17:05
-     * @param string $Username
-     * @param string $ReposName
-     * @param string $Path
-     * @return mixed
-     */
-    public function getReposPath(string $Username, string $ReposName, string $Path)
-    {
-        //var_dump(func_get_args());
-        $temp = self::SendApi('/repos/' . $Username . '/' . $ReposName . '/contents' . $Path);
-        //var_dump($temp);
-        return json_decode($temp);
-    }
 }
