2b91a9242d3d8183196f0044fa63539c8c7ab29b
手瓜一十雪 <nineto0@163.com>
2023-05-14 23:41:56 +0800

    Helpet Op

--- Helper.php
+++ Helper.php
@@ -1,40 +1,51 @@
 <?php
 namespace TypechoPlugin\GithubFile;
+
 /**
- *插件辅助功能实现部分
- * Class类名称(GithubFile_Helper)
+ * 插件辅助功能类 Helper
+ *
  * @package GithubFile
- * @author Mlikiowa<nineto0@163.com>
- * @version 1.1.0
- * @since 1.0.0
+ * @version 1.4.0
+ * @link https://github.com/Mlikiowa/typecho-github-file/issues
  */
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+
 class Helper
 {
-    public static function GetConfig($name, $default)
+    /**
+     * 获取指定键对应的配置值
+     * @param string $key 键名
+     * @param mixed|null $default 默认值（允许为空）
+     * @return mixed 配置值或默认值
+     */
+    public static function getConfig($key, $default = null)
     {
-        static $result = true;
-        if ($result === true) {
-            $_db = \Typecho\Db::get();
-            $result = $_db->fetchAll($_db->select('value')->from('table.options')->where('name = ?', 'plugin:GithubFile'));
+        static $config = null;
+        if ($config === null) {
+            // 初始化插件配置
+            $config = Helper::options()->plugin('GithubFile')->toArray();
         }
-        if (!isset($result[0]['value'])) {
-            return $default;
-        }
-        $_options = unserialize($result[0]['value']);
-        return ($_options[$name] ?? $default);
-    }
-   public static function replaceCode($string,$replace)
-   {
-       $regex = "/\[(.*?)\]/";
-       preg_match_all($regex, $string, $matches);
-       for($i = 0; $i < count($matches[1]); $i++)
-       {
-           $match = $matches[1][$i];
-           $newValue = $replace[$match];
-           $string = str_replace($matches[0][$i], $newValue, $string);
-       }
-       return $string;
-   }
 
+        // 获取指定键对应的配置值
+        return isset($config[$key]) ? $config[$key] : $default;
+    }
+
+    /**
+     * 根据数组中的键值替换文本中相应的变量名
+     *
+     * @param string $content 源文本
+     * @param array $array 替换用的数组
+     * @return string 替换后的文本
+     */
+    public static function replaceCode($content, $array)
+    {
+        // 将数组中的键值对转换为变量和值
+        extract($array);
+        // 获取数组中的键名，并给键名加上花括号
+        $keys = array_map(function ($key) {
+            return '[' . $key . ']';
+        }, array_keys($array));
+        // 替换文本中的变量名，返回替换后的文本
+        return str_replace($keys, array_values($array), $content);
+    }
 }
